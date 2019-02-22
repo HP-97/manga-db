bed95ea488ef03ca92866d5dc2981325b69fb047
HoaP123 <hqpha3@student.monash.edu>
2019-02-22 19:10:53 +1100

    Added small manga-app changes

--- manga/models.py
+++ manga/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Manga(models.Model):
-	title = models.CharField(max_length=200, unique=True)
 
-	author = models.CharField()
+	title = models.CharField(max_length=200, unique=True)
+	author = models.CharField(max_length=200)
+	pub_status = models.CharField(max_length=100)
+
+# TODO: Complete MangaGenre model.
+class MangaGenre(models.Model):
+
+
+
